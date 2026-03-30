29c7499fc29dab56d727f26c49e709aff416f4d2
Sudheendra Ayyalasomayajula <sudhee@icloud.com>
2026-03-30 00:03:16 -0400

    minigrep and closures

---
+++ closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+C:\Users\sudhe\OneDrive\Desktop\rust\closures\target\debug\closures.exe: C:\Users\sudhe\OneDrive\Desktop\rust\closures\src\main.rs
---
+++ closures/target/debug/deps/closures.d
@@ -0,0 +1,5 @@
+C:\Users\sudhe\OneDrive\Desktop\rust\closures\target\debug\deps\closures.d: src\main.rs
+
+C:\Users\sudhe\OneDrive\Desktop\rust\closures\target\debug\deps\closures.exe: src\main.rs
+
+src\main.rs:
